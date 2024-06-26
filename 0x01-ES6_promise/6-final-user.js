846e81ef7653734ddc9b065697e39ab80a755372
The Primotion Studio <oyedelenewton@gmail.com>
2024-06-26 14:59:52 +0100

    "Updated loadBalancer function to return a Promise.race and added a new guardrail function that wraps a math function in a try-catch-finally block."

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastname, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastname), uploadPhoto(fileName)])
+    .then((response) => {
+      response.map((res) => ({
+        status: res.status,
+        value: (res.status === 'fulfilled') ? res.value : res.reason,
+      }));
+    });
+}
--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+export default function divideFunction(numerator, denominator) {
+  if (parseInt(denominator, 10) === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return parseInt(numerator, 10) / parseInt(denominator, 10);
+}
