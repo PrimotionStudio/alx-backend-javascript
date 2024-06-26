b9dd5148890180ab4ca7981cad59e89edd1ace05
The Primotion Studio <oyedelenewton@gmail.com>
2024-06-26 04:04:35 +0100

    "Updated promise handling in 2-then.js, added new function handleProfileSignup in 3-all.js, and refactored utils.js"

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()]).then((res) => {
+    const [pic, user] = res;
+    console.log(`${pic.body} ${user.firstName} ${user.lastName}`);
+  });
+}
