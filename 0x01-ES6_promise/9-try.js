61a826de3269159456fd728b70fd28883ac268c8
The Primotion Studio <oyedelenewton@gmail.com>
2024-06-26 15:00:04 +0100

    "Updated loadBalancer function to return a Promise.race and added a new guardrail function that wraps a math function in a try-catch-finally block."

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+export default function guardrail(mathFunction) {
+  const maths = [];
+  try {
+    maths.push(mathFunction());
+  } catch (error) {
+    maths.push(error.toString());
+  } finally {
+    maths.push('Guardrail was processed');
+  }
+  return maths;
+}
