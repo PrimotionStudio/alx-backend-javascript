fdf592a1c35479a04452d82add1d170618518fa4
The Primotion Studio <oyedelenewton@gmail.com>
2024-06-27 03:36:07 +0100

    "Added console log statement and changed res.reason to String(res.reason) in handleProfileSignup function"

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,9 +6,10 @@ export default function handleProfileSignup(firstName, lastname, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastname), uploadPhoto(fileName)])
     .then((response) => {
+      console.log(response);
       response.map((res) => ({
         status: res.status,
-        value: (res.status === 'fulfilled') ? res.value : res.reason.toString(),
+        value: (res.status === 'fulfilled') ? res.value : String(res.reason),
       }));
     });
 }
