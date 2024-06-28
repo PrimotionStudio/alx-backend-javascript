b7c8b2a51083644ba41696ba5bb9e6616952c4ee
The Primotion Studio <oyedelenewton@gmail.com>
2024-06-28 18:17:10 +0100

    "Remove comments and newline from ClassRoom class definition"

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
-// Define a default export class named ClassRoom
 class ClassRoom {
-    // Constructor function that is called when
-    // an object is created from the class
     constructor(maxStudentsSize) {
-        // Initialize an instance variable
-        // _maxStudentsSize with the value of maxStudentsSize
         this._maxStudentsSize = maxStudentsSize;
     }
 }
-
 export default ClassRoom;
