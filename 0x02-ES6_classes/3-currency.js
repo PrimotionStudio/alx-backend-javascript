1b31772d733399ba74ae4cf049849d2a659c3ae7
The Primotion Studio <oyedelenewton@gmail.com>
2024-06-29 03:11:32 +0100

    "Added Currency class with constructor, getters, setters, and displayFullCurrency method"

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,32 @@
-#!/usr/bin/node
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(c) {
+    if (typeof c !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = c;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(n) {
+    if (typeof n !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = n;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
