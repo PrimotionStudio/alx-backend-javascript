9a50d9301675c5fe85e750061b51483c3f0cce0a
The Primotion Studio <oyedelenewton@gmail.com>
2024-06-29 02:54:04 +0100

    "Added ES6 classes for Car and HolbertonCourse with constructors and methods"

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default class Car {
+  constructor(brand, motor, _color) {
+    this._brand = brand;
+    this._motor = motor;
+    this.color = _color;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this.color);
+  }
+}
--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        try {
+            if (typeof(name) === 'string' )
+        } catch (error) {
+            
+        }
+    }
+}
