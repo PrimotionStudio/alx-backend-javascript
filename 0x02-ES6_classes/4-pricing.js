092acb7f512697b5122d28279120e2d22bc1e245
The Primotion Studio <oyedelenewton@gmail.com>
2024-06-29 03:25:05 +0100

    "Added Pricing class with constructor, getters/setters, and methods for displaying and converting prices"

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,1 +1,40 @@
-#!/usr/bin/node
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(a) {
+    if (typeof a !== 'number' || a < 0) {
+      throw new TypeError('Amount must be a positive number');
+    }
+    this._amount = a;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(c) {
+    if (!(c instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency class');
+    }
+    this._currency = c;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Amount and ConversionRate must be Numbers');
+    }
+  }
+}
