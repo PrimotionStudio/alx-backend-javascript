1ea3c7b8a748e244f09b27f47307a2b92db638a3
The Primotion Studio <oyedelenewton@gmail.com>
2024-06-29 03:34:47 +0100

    "Added getter and setter methods for brand, motor, and color properties in Car class; updated cloneCar method to use constructor"

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -2,10 +2,43 @@ export default class Car {
   constructor(brand, motor, _color) {
     this._brand = brand;
     this._motor = motor;
-    this.color = _color;
+    this._color = _color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(b) {
+    if (typeof b !== 'string') {
+      throw new TypeError('Brand must be a string');
+    }
+    this._brand = b;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(m) {
+    if (typeof m !== 'number') {
+      throw new TypeError('Motor must be a number');
+    }
+    this._motor = m;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(c) {
+    if (typeof c !== 'string') {
+      throw new TypeError('Color must be a string');
+    }
+    this._color = c;
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this.color);
+    return new this.constructor();
   }
 }
