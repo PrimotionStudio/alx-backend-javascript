b0d9ad128d0fb5285b843afc4b793a588f7a9252
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-01 21:39:43 +0100

    "Added import and function call in 10-main.js, and updated 10-update_uniq_items.js to export a function that updates a Map"

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+export default function updateUniqueItems(_map) {
+  if (!(_map instanceof Map)) throw new Error('Cannot process');
+  _map.forEach((v, k) => {
+    if (v === 1) _map.set(k, 100);
+  });
+  return _map;
+}
