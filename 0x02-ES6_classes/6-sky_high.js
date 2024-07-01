3def2939c84a14bf0683e2b375a0f6382d66da05
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-02 00:44:47 +0100

    "Added setter for 'floors' property in SkyHighBuilding class"

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -10,6 +10,13 @@ export default class SkyHighBuilding extends Building {
     return this._floors;
   }
 
+  set floors(floor) {
+    if (typeof floor !== 'number' || floor < 1 || floor > this._floors) {
+      throw new RangeError('Number of floors must be between 1 and the total number of floors');
+    }
+    this._floors = floor;
+  }
+
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
   }
