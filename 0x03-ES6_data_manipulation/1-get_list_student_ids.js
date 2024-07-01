ef9e9ca65297a9cb3875c763fd1ee8f44b286cea
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-01 18:22:50 +0100

    "Updated code formatting and imports in multiple files"

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,4 @@
 export default function getListStudentIds(students) {
-	if (!students.isArray()) return [];
-	return students.map((student) => student.id);
+  if (!Array.isArray(students)) return [];
+  return students.map((student) => student.id);
 }
--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,1 +1,3 @@
-#!/usr/bin/node
+export default function getStudentsByLocation(students) {
+  return [];
+}
