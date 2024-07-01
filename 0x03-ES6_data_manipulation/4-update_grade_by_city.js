12aaf244ee5a415a86c20a76359b5ae0e54cad05
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-01 19:23:23 +0100

    "Added function updateStudentGradeByCity to update student grades by city"

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,23 @@
-#!/usr/bin/node
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const studentsInCity = students.filter((student) => student.location === city);
+  return studentsInCity.map((student) => {
+    const studentInCityGrade = newGrades.find((grade) => student.id === grade.studentId);
+    let newStudent;
+    if (!studentInCityGrade) {
+      newStudent = {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: 'N/A',
+      };
+    } else {
+      newStudent = {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: studentInCityGrade.grade,
+      };
+    }
+    return newStudent;
+  });
+}
