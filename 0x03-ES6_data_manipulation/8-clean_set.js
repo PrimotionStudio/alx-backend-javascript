64bf506f14968355fa42287ee0ad3175c8342117
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-01 21:06:47 +0100

    "Added cleanSet function and main script to test it"

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+export default function cleanSet(_set, _str) {
+  let _cleanSet = '';
+  if (_str === '') return _cleanSet;
+  _set.forEach((element) => {
+    if (element.startsWith(_str)) {
+      const index = _str.length;
+      if (_cleanSet !== '') _cleanSet += '-';
+      _cleanSet += element.slice(index);
+    }
+  });
+  return _cleanSet;
+}
