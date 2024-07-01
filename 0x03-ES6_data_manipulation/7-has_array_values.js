fa364435f56fa726bd1d180c51bb7c07479096f1
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-01 20:39:19 +0100

    "Added hasValuesFromArray function and main.js file to test it"

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function hasValuesFromArray(_set, _arr) {
+  for (const arrItem of _arr) {
+    if (!_set.has(arrItem)) {
+      return false;
+    }
+  }
+  return true;
+}
