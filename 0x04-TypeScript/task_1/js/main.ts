1faa261ac297aa655eab256027bb477f345e9ad6
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-03 15:53:13 +0100

    "Added TypeScript configuration files, updated package.json, and created webpack.config.js for tasks 1, 2, 4, and 5."

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,3 +14,25 @@ interface Directors extends Teacher {
 function printTeacher(firstName:string, lastName:string): string {
     return `${firstName[0]}. ${lastName}`;
 }
+
+interface studentClass {
+    firstName: string;
+    lastName: string;
+    workOnHomework: () => string;
+    displayName: () => string;
+}
+
+class StudentClass {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomeWork(): string {
+        return 'Currently working';
+    };
+    displayName(): string {
+        return this.firstName;
+    }
+}
