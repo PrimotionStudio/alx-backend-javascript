37377c1a6869386c873feaf530cf089781d87c4d
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-05 01:41:34 +0100

    deleted all the python code from this repo

--- 0x00-python_variable_annotations/2-floor.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""A module containing a single function to
-calculate the floor of a given number."""
-import math
-
-
-def floor(n: float) -> int:
-    """Returns the largest integer not greater than the input number."""
-    return math.floor(n)
