23795138618738440a65b2e854866fdf68f79147
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-19 12:55:48 +0100

    back to alx

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+  process.stdout.write('This important software is now closing');
+});
