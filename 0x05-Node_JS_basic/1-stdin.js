e762844181a5eb0e864da80d525d959308b0c47e
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-19 21:01:19 +0100

    "Added closing bracket and 'end' event handler to stdin"

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,5 +7,7 @@ process.stdin.on('readable', () => {
   if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
+});
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing');
 });
