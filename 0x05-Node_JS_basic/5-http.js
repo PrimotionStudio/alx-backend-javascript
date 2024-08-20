6f4804ae984d1c7ffdc9b0bceb638aca0cc78a9f
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-20 02:01:35 +0100

    "Added HTTP server implementation using native Node.js http module and Express.js framework"

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,1 +1,41 @@
 #!/usr/bin/node
+const http = require('http');
+const url = require('url');
+const fs = require('fs').promises;
+
+const path = process.argv.length === 2 ? process.argv[1] : '';
+const app = http.createServer((req, res) => {
+  const reqUrl = url.parse(req.url).pathname;
+  res.writeHead(200, { 'Content-type': 'text/plain' });
+  if (reqUrl === '/') {
+    res.write('Hello Holberton School!\n');
+  } else if (reqUrl === '/students') {
+    res.write('This is the list of our students\n');
+    fs.readFile(path, { encoding: 'utf-8' })
+      .then((content) => {
+        const cs = [];
+        const swe = [];
+        const students = content.split('\n');
+        students.splice(0, 1);
+        students.forEach((student) => {
+          const stud = student.split(',');
+          if (stud[3] === 'CS') {
+            cs.push(stud[0]);
+          } else if (stud[3] === 'SWE') {
+            swe.push(stud[0]);
+          }
+        });
+        res.write(`Number of students: ${cs.length + swe.length}\n`);
+        res.write(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}\n`);
+        res.write(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}\n`);
+        res.end();
+      })
+      .catch(() => {
+        res.write('Cannot load the database\n');
+        res.end();
+      });
+  } else {
+    res.end();
+  }
+}).listen(1245);
+module.exports = app;
