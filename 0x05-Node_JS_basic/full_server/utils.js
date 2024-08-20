2f35120ceaca11cb7ac2153a6f6573db68552ceb
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-20 19:16:08 +0100

    "Added utils.js file with readDatabase function to read and parse a database file"

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+import fs from 'fs/promises';
+
+export async function readDatabase(path) {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    const lines = data.trim().split('\n');
+    const fields = {};
+    lines.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (!fields[field]) fields[field] = [];
+      fields[field].push(firstname);
+    });
+    return fields;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
