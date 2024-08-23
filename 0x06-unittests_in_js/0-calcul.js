1fc0cd282d81e66583a6f5870d72a2494526a093
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-23 11:51:10 +0100

    first commit

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,3 +1,1 @@
 #!/usr/bin/node
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
-module.exports = calculateNumber;
--- 0x06-unittests_in_js/myFunction.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-export default function doubleNumber(number) {
-    if (typeof number !== 'number') {
-        throw new Error('Input must be a number');
-    }
-    return number * 2;
-}
