1c2ef6d47b52db69e30cb48e923a66fe1dbe0d40
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-23 14:04:53 +0100

    "Updated api.js to conditionally start the server and added a package.json file with dependencies and scripts"

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -7,8 +7,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to the payment system");
 });
 
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
-});
+if (require.main === module) {
+  app.listen(7865, () => {
+    console.log("API available on localhost port 7865");
+  });
+}
 
 module.exports = app;
